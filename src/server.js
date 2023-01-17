3e2512e0cdceb533cbee927a6e010dd8af1c1181
Francisco-Fortes <fortesfco@gmail.com>
2023-01-17 15:23:09 +0100

    Server & Mongo connection added

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const server = express();
+const port = process.env.PORT || 3001;
+//if process.env.PORT does not work, we will use 3001
+
+//Middlewares
+server.use(cors());
+server.use(express.json());
+//Endpoints
+
+//ErrorHandlers
+
+mongoose.set("strictQuery", false);
+//Why we are using this?
+mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+//It only takes one parameter, the connection string (URL) from Mongo
+
+mongoose.connection.on("connected", () => {
+  //Once the connection is done run the server
+  console.log(`Connected to Mongo`);
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`Server running on Port: ${port}`);
+  });
+});
